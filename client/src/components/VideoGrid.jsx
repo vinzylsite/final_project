95ec4fda8d99cedddb4840398e733ba0e00786b7
RhodmarQ <rhodmarquitlong@gmail.com>
2026-03-12 22:10:28 +0800

    Update VideoGrid.jsx

--- client/src/components/VideoGrid.jsx
+++ client/src/components/VideoGrid.jsx
@@ -3,7 +3,7 @@ import VideoCard from './VideoCard';
 
 export default function VideoGrid({ videos }) {
   return (
-    <Grid container spacing={2} sx={{ justifyContent: 'left' }}>
+    <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
       {videos.map((v) => (
         <Grid item key={v.id} xs={12} sm={6} md={4} lg={3}>
           <VideoCard video={v} />
