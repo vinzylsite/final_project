fc7e4eba7bcc3250cc0a13068c2e54bcf8219c39
Jonald Hermogeno <jonaldhermogeno625@gmail.com>
2026-03-21 21:49:10 +0800

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
           '&:before': {
             content: '""',
             position: 'absolute',
-            top: 0,
+            top: 0;
             left: '-100%',
             width: '100%',
             height: '100%',
